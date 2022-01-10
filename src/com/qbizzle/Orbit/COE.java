04b9b6f536c8d98245eb49839acb7febf725904d
qbarnes <quinton.barnes68@gmail.com>
2022-01-10 01:22:36 -0600

    can create COE from two state vectors

--- src/com/qbizzle/Orbit/COE.java
+++ src/com/qbizzle/Orbit/COE.java
@@ -1,4 +1,5 @@
 package com.qbizzle.Orbit;
+import com.qbizzle.Math.Vector;
 import com.qbizzle.Orbit.OrbitalMath;
 
 public class COE {
@@ -22,6 +23,28 @@ public class COE {
         m_aop = tle.AOP();
         m_inclination = tle.Inclination();
         m_trueAnomaly = OrbitalMath.Mean2True(meanMotion, m_eccentricity);
+    }
+
+    public COE(Vector position, Vector velocity) {
+        Vector angMomentum = position.cross(velocity);
+//        pointing towards the ascending node
+        Vector lineOfNodes = angMomentum.cross(new Vector(0, 0, 1.0)).norm();
+        Vector eccVector = eccentricVector(position, velocity);
+        m_eccentricity = eccVector.mag();
+        m_inclination = Math.acos(angMomentum.z() / angMomentum.mag());
+        m_lan = Math.acos(lineOfNodes.x() / lineOfNodes.mag());
+        if (lineOfNodes.y() < 0) m_lan = 360 - m_lan;
+        m_aop = Math.acos(lineOfNodes.dot(eccVector) / (lineOfNodes.mag() * eccVector.mag()));
+        if (eccVector.z() < 0) m_aop = 360 - m_aop;
+        m_trueAnomaly = Math.acos(eccVector.dot(position) / (eccVector.mag() * position.mag()));
+        if (position.dot(velocity) < 0) m_trueAnomaly = 360 - m_trueAnomaly;
+        m_semiMajorAxis = (Math.pow(angMomentum.mag(), 2) * (1 - Math.pow(m_eccentricity, 2))) / OrbitalMath.MU;
+    }
+
+    private Vector eccentricVector(Vector p, Vector v) {
+        Vector rtn = v.scale(p.dot(v));
+        rtn = p.scale(Math.pow(v.mag(), 2) - (OrbitalMath.MU / p.mag())).minus(rtn);
+        return rtn.scale(1.0 / OrbitalMath.MU);
     }
 
 }
