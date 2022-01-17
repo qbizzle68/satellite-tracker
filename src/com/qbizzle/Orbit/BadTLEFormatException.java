f12292396577edc3cfe27377bedf9678a961dcb3
qbarnes <quinton.barnes68@gmail.com>
2022-01-17 15:14:18 -0600

    added doxygen documentation to BadTLEFormatException

--- src/com/qbizzle/Orbit/BadTLEFormatException.java
+++ src/com/qbizzle/Orbit/BadTLEFormatException.java
@@ -1,5 +1,14 @@
+/** @file
+ * This file contains the BadTLEFormatException class, which is
+ * used by the TLE class to alert to a badly formatted TLE text.
+ */
+
 package com.qbizzle.Orbit;
 
+/**
+ * This exception class is used to signal a bad formatted TLE string.
+ * @todo possibly derive more descriptive exceptions that extend this class
+ */
 public class BadTLEFormatException extends Exception {
     public BadTLEFormatException(String errorMessage) {
         super(errorMessage);
