f78fe6d13eb75c99ccc9d13105c9eaf4345fdc2e
qbarnes <quinton.barnes68@gmail.com>
2022-01-16 22:18:38 -0600

    deleted unused methods and documented code for doxygen

--- src/com/qbizzle/Math/JD.java
+++ src/com/qbizzle/Math/JD.java
@@ -1,4 +1,4 @@
-/**
+/** @file
  * This file contains a Julian Date class to help facilitate with differentiating time.
  * Since any date (day, month, year) can be quickly converted to a Julian Date the largest
  * unit of time that needs to be handled should be a solar day (86,400.0 seconds), with hours,
@@ -14,35 +14,44 @@ package com.qbizzle.Math;
 import com.qbizzle.Orbit.TLE;
 import java.util.*;
 
+/**
+ * A Julian Day class used to handle time computations. The JD class internally stores the Julian Day
+ * for a specific date and time and can quickly compute differences between other Julian Days, or
+ * find a future or past Julian Day given a time difference. The main unit of time considered with
+ * Julian Days is the solar day.
+ * @note A solar day is equivalent to 24 hours, 1440 minutes or 86400 seconds.
+ */
 public class JD {
     private final double m_julianDate;
-    /**
+    /*
      * static final rates of various time quantities per day
      */
-    public static final double SECONDSPERDAY = 86_400.0;
+    /// Number of seconds in a solar day.
+    public static final double SECONDSPERDAY = 86400.0;
+    /// Number of minutes in a solar day.
     public static final double MINUTESPERDAY = 1440.0;
+    /// Number of hours in a solar day.
     public static final double HOURSPERDAY = 24.0;
 
-    /**
-     * Constructs the Julian Date from the GregorianCalendar object
-     * @param gcal GregorianCalendar that the Julian Day is meant to represent. Ideal
-     *             for constructing a certain date and time (using GregorianCalendar's constructor)
+    /** Constructs the Julian Date from the Java GregorianCalendar object.
+     * @param GregCal GregorianCalendar instance that represents the Julian Date.
      */
-    public JD(GregorianCalendar gcal) {
+//    This is ideal if the GregorianCalendar can handle error checking of the inputs.
+//    If not we may as well implement our own constructor similar to the GetDate method.
+    public JD(GregorianCalendar GregCal) {
         m_julianDate = GetDate(
-                gcal.get(Calendar.MONTH),
-                gcal.get(Calendar.DATE),
-                gcal.get(Calendar.YEAR),
-                gcal.get(Calendar.HOUR_OF_DAY),
-                gcal.get(Calendar.MINUTE),
-                gcal.get(Calendar.SECOND)
+                GregCal.get(Calendar.MONTH),
+                GregCal.get(Calendar.DATE),
+                GregCal.get(Calendar.YEAR),
+                GregCal.get(Calendar.HOUR_OF_DAY),
+                GregCal.get(Calendar.MINUTE),
+                GregCal.get(Calendar.SECOND)
         );
     }
 
-    /**
-     * Constructs the Julian Date from the Date object
+    /** Constructs the Julian Date from the Java Date object.
      * @param date Date object that the Julian Day is meant to represent. Ideal
-     *             for using the now() method to compute current satellite position.
+     *             for using the now() method to compute current satellite positions.
      */
     public JD(Date date) {
         String[] strTokens = date.toString().split("[\s]");
@@ -56,85 +65,99 @@ public class JD {
         );
     }
 
-    /**
-     * Constructs the Julian Date from the epoch element from a TLE. Ideal for computing
+    /**Constructs the Julian Date from the epoch element of a TLE./ Ideal for computing
      * future positions from the TLE epoch.
-     * @param tle TLE with the epoch to create a Julian Date from
+     * @param tle @link com.qbizzle.Orbit.TLE TLE @endlink that contains the epoch used to create
+     *            a Julian Date.
      */
     public JD(TLE tle) {
         m_julianDate = GetDate(12, 31, tle.EpochYear()-1, 0, 0, 0) + tle.EpochDay();
     }
 
-    /**
-     * Constructs the Julian Date directly from a known Julian Date number
-     * @param julianDate the Julian Date in double form.
+    /** Constructs the Julian Date directly from a known Julian Date number
+     * @param julianDate A known Julian Date.
      */
     public JD(double julianDate) {
         m_julianDate = julianDate;
     }
 
-    /**
-     * Turns elements of a Gregorian Date into a Julian Date as described by the algorithm
-     * found at "https://en.wikipedia.org/wiki/Julian_day#Converting_Gregorian_calendar_date_to_Julian_Day_Number"
-     * @param m Gregorian month
-     * @param d day of the month
-     * @param y year, a value for a year in BCE should be (1 - year(BCE))
-     * @param h hour of the day [0-24) (note: a value greater than 23 would add more days to the date)
-     * @param min minute of the hour [0-59] (note: a value greater than 59 would add more hours to the date)
-     * @param s second of the minute [0-59] (note: a value greater than 59 would add more minutes to the date)
-     * @return Julian Date associated with the date and time of the parameters
+    /** Converts values of a Gregorian Date into a Julian Date.
+     * @param mon Month number of the year, starting with 1.
+     * @param day Day of the month.
+     * @param yr Year relative to 1 AD.
+     * @param hr Hour of the day in 24 hour notation.
+     * @param min Minute of the hour.
+     * @param sec Seconds, can include additional fractions of a second.
+     * @return The number of days since 12:00:00 1/1/4713 BCE.
+     * @note The algorithm used for converting dates can be found
+     *     <a href="https://en.wikipedia.org/wiki/Julian_day#Converting_Gregorian_calendar_date_to_Julian_Day_Number">
+     *     here</a>.
      */
-    public static double GetDate(int m, int d, int y, int h, int min, int s) {
-        int julianNumber = (1461 * (y + 4800 + (m - 14)/12))/4 + (367 * (m - 2 - 12 * ((m - 14)/12)))/12 - (3 * ((y + 4900 + (m - 14)/12)/100))/4 + d - 32075;
-        return julianNumber + ((h - 12) / 24.0) + (min / 1440.0) + (s / 86400.0);
+    public static double GetDate(int mon, int day, int yr, int hr, int min, double sec) {
+        int julianNumber = (1461 * (yr + 4800 + (mon - 14)/12))/4 + (367 * (mon - 2 - 12 * ((mon - 14)/12)))/12 - (3 * ((yr + 4900 + (mon - 14)/12)/100))/4 + day - 32075;
+        return julianNumber + ((hr - 12) / 24.0) + (min / 1440.0) + (sec / 86400.0);
     }
 
-    /**
-     * @return the value of the Julian Date this object was constructed by
+    /** @name Getter methods.
+     * Methods to retrieve values associated with the current Julian Date.
+     */
+    /**@{*/
+    /** Getter method for retrieving the Julian Date value.
+     * @return The Julian Date value associated with this instance.
      */
     public double Value() {
         return m_julianDate;
     }
 
-    /**
-     * @return the integer portion of the Julian Date
+    /** Getter method for retrieving the Julian Date Number.
+     * @return The Julian Day number associated with this instance.
+     * @note This is the integer portion of @link com.qbizzle.Math.JD.Value() value. @endlink
      */
     public int Number() {
         return (int) m_julianDate;
     }
 
-    /**
-     * @return the fractional portion of the Julian Date (time past 12:00:00 of the given day)
+    /** Getter method for retrieving the Julian Date fractional part.
+     * @return The fractional portion of the Julian Date.
+     * @note This is not the fractional time of the day, but the fractional portion of the
+     * 24-hour period following 12pm of the associated day.
      */
     public double Fraction() {
         return m_julianDate - (int) m_julianDate;
     }
+    /**@}*/
 
-    /**
-     * Computes the difference between two Julian Dates in solar days
-     * @param jd the Julian Date in which to find the difference
-     * @return the number of solar days between the two dates. A negative
-     *      number indicates the jd param is in the future
+    /** @name Computation methods.
+     * Methods used to compute values between or to create new Julian Dates.
+     */
+    /**@{*/
+    /** Computes the difference between two Julian Dates in solar days.
+     * @param jd The Julian Date in which to find the difference.
+     * @return The number of solar days between the two dates.
+     * @note A negative return value indicates the @p jd parameter date occurs before
+     * this instance date.
      */
     public double Difference(JD jd) {
         return m_julianDate - jd.m_julianDate;
     }
 
-    /**
-     * Computes a Julian Date in the past or future from the current date.
-     * @param days number of solar days added to current date. a negative number will return
-     *             a date in the past from the current date.
-     * @return a Julian Date equal to the current date plus the days param.
+    /** Computes a Julian Date relative to the current instance.
+     * @param days Number of solar days added to current date.
+     * @return A Julian Date equal to the current date plus the @p days param.
+     * @note A negative value for @p days will return a Julian Date that preceeds
+     * the date of the current instance.
      */
     public JD Future(double days) {
         return new JD(m_julianDate + days);
     }
+    /**@}*/
 
-    /**
-     * helper method to return the month value from the string value returned by the java
-     * Date class.
-     * @param str string abbreviation of the month gotten from the Date.toString() method
-     * @return the integer number of the month referring to the str param
+    /** Get the month number of the abreviated string.
+     * A helper method to convert the string abreviation from the Java Date class to the
+     * corresponding month number.
+     * @param str The string abbreviation of the month gotten from the
+     *            <a href="https://docs.oracle.com/javase/8/docs/api/java/util/Date.html#toString--">Date.toString()</a> method.
+     * @return The integer number of the month referring to the @p str parameter.
      */
     private static int MonthTable(String str) {
         return switch (str) {
@@ -151,62 +174,5 @@ public class JD {
             case "Dec" -> 12;
             default -> 1;
         };
-    }
-
-    /**
-     * Computes what month contains the day of the given year. The year is needed to determine
-     * how many days are in February (leap year)
-     * @param day day number of the year
-     * @param year year in order to determine length of February
-     * @return the month number which contains the day of the year
-     */
-    private static int MonthHasDay(int day, int year) {
-        int month = 1;
-        while (true) {
-            int dperm = DaysPerMonth(month, year);
-            if (day > dperm) {
-                day -= dperm;
-                month++;
-            }
-            else return month;
-        }
-    }
-
-    /**
-     * returns the number of days in a month using year to determine leap year
-     * @param month month number of the year
-     * @param year year number in order to determine February
-     * @return the number of days in the requested month
-     */
-    public static int DaysPerMonth(int month, int year) {
-        switch (month) {
-            default:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                return 31;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                return 30;
-            case 2:
-                return 28 + ((((year%4 == 0) && (year%100 != 0)) || (year%400 == 0)) ? 1 : 0);
-        }
-    }
-
-    /**
-     * Computes the number of days in the year that precede a given month of the year.
-     * @param month month number
-     * @param year needed to determine number of days in February
-     * @return number of days that occur before the first of the month param
-     */
-    private static int DaysPrecedingMonth(int month, int year) {
-        int daySum = 0;
-        for (int i = 1; i < month; i++) daySum += DaysPerMonth(i, year);
-        return daySum;
     }
 }
