14a4c099c96f177a9355f527ea670849fe78b547
qbarnes <quinton.barnes68@gmail.com>
2022-01-25 17:14:58 -0600

    added documentation to matrix class for doxygen

--- src/com/qbizzle/Math/Matrix.java
+++ src/com/qbizzle/Math/Matrix.java
@@ -12,6 +12,7 @@ package com.qbizzle.Math;
 public class Matrix implements Cloneable {
     double[][] m_data = new double[3][3];
 
+    /** Default constructor, that sets each matrix component to 0.0. */
     public Matrix() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -20,18 +21,38 @@ public class Matrix implements Cloneable {
         }
     }
 
+    /** Column Vector constructor that sets each column of the matrix to its
+     * relative column vector in the argument list. This constructor is ideal
+     * for creating a rotation matrix where you know where you want the i, j
+     * and k unit vectors to map to.
+     * @param column1 Column Vector that the i unit vector maps to.
+     * @param column2 Column Vector that the j unit vector maps to.
+     * @param column3 Column Vector that the k unit vector maps to.
+     */
     public Matrix(Vector column1, Vector column2, Vector column3) {
         m_data[0][0] = column1.x(); m_data[0][1] = column1.y(); m_data[0][2] = column1.z();
         m_data[1][0] = column2.x(); m_data[1][1] = column2.y(); m_data[1][2] = column2.z();
         m_data[2][0] = column3.x(); m_data[2][1] = column3.y(); m_data[2][2] = column3.z();
     }
 
+    /// @name Overloaded methods.
+    /// Methods that are inherited from Java Cloneable class.
+///@{
+
+    /** Returns a string representation of the matrix.
+     * @return A string representation of the matrix.
+     */
     public String toString() {
         return "[ " + m_data[0][0] + ", " + m_data[0][1] + ", " + m_data[0][2] + "\n  "
                 + m_data[1][0] + ", " + m_data[1][1] + ", " + m_data[1][2] + "\n  "
                 + m_data[2][0] + ", " + m_data[2][1] + ", " + m_data[2][2] + " ]";
     }
 
+    /** Indicates whether a Matrix is 'equal to' this instance.
+     * @param obj Matrix to compare this matrix to.
+     * @return True if this object is the same as @p obj argument, false otherwise.
+     * @throws IllegalArgumentException if @p obj argument is not of type Matrix.
+     */
     public boolean equals(Object obj) {
         if (obj instanceof Matrix rhs) {
             for (int i = 0; i < 3; i++) {
@@ -44,6 +65,12 @@ public class Matrix implements Cloneable {
         throw new IllegalArgumentException("Object is not of type 'Matrix'");
     }
 
+    /** Creates and returns a copy of this object.
+     * A deep copy of the matrix is made before returning.
+     * @return A clone of this instance.
+     * @warning This method returns a Java Object, so explicit casting may be necessary
+     * to avoid errors.
+     */
     public Object clone() {
         try {
             Matrix tmp = (Matrix)super.clone();
@@ -57,18 +84,44 @@ public class Matrix implements Cloneable {
         }
     }
 
+///@}
+
+    /** Retrieves a certain component of the matrix by index (starting at 0).
+     * @param row Row index of desired element.
+     * @param col Column index of desired element.
+     * @return The value at the indexed position.
+     * @throws ArrayIndexOutOfBoundsException if either of the indexes are out
+     * of range (indexing starts at 0).
+     */
     public double get(int row, int col) {
         return m_data[row][col];
     }
 
+    /** Sets a certain component of the matrix by index (starting at 0).
+     * @param row Row index of element to modify.
+     * @param col Column index of element to modify.
+     * @param value Value to set the desired component to.
+     * @throws ArrayIndexOutOfBoundsException if either of the indexes are out
+     * of range (indexing starts at 0).
+     */
     public void set(int row, int col, double value) {
         m_data[row][col] = value;
     }
 
+    /** Retrieves a certain column of the matrix by index (starting at 0).
+     * @param col Column index of desired column vector.
+     * @return The desired column vector of the matrix.
+     * @throws ArrayIndexOutOfBoundsException if column index is out of range
+     * (indexing starts at 0).
+     */
     public Vector getColumn(int col) {
         return new Vector(m_data[0][col], m_data[1][col], m_data[2][col]);
     }
 
+    /** Matrix addition operator.
+     * @param rhs Matrix to be added to this matrix.
+     * @return The Matrix sum of this instance and @p rhs argument.
+     */
     public Matrix plus(Matrix rhs) {
         Matrix rtn = new Matrix();
         for (int i = 0; i < 3; i++) {
@@ -79,6 +132,10 @@ public class Matrix implements Cloneable {
         return rtn;
     }
 
+    /** Matrix addition assignment operator.
+     * @param rhs Matrix to be added to this matrix.
+     * @return A clone of this instance after the addition.
+     */
     public Matrix pluseEquals(Matrix rhs) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -88,6 +145,23 @@ public class Matrix implements Cloneable {
         return (Matrix)this.clone();
     }
 
+    /** Unary subtraction operator.
+     * @return A matrix with negated component values of this instance.
+     */
+    public Matrix minus() {
+        Matrix rtn = new Matrix();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                rtn.m_data[i][j] = this.m_data[i][j];
+            }
+        }
+        return rtn;
+    }
+
+    /** Matrix subtraction operator.
+     * @param rhs Matrix to subtract from this instance.
+     * @return A Matrix equal to this instance minus @p rhs.
+     */
     public Matrix minus(Matrix rhs) {
         Matrix rtn = new Matrix();
         for (int i = 0; i < 3; i++) {
@@ -98,6 +172,10 @@ public class Matrix implements Cloneable {
         return rtn;
     }
 
+    /** Matrix subtraction assignment operator.
+     * @param rhs Matrix to subtract from this instance.
+     * @return A clone of this instance after it has been subtracted.
+     */
     public Matrix minusEquals(Matrix rhs) {
         Matrix rtn = new Matrix();
         for (int i = 0; i < 3; i++) {
@@ -108,6 +186,10 @@ public class Matrix implements Cloneable {
         return (Matrix)this.clone();
     }
 
+    /** Scalar multiplication operator.
+     * @param lambda Scalar to multiply each matrix component by.
+     * @return A Matrix with all components multiplies by @p lambda.
+     */
     public Matrix scale(double lambda) {
         Matrix rtn = new Matrix();
         for (int i = 0; i < 3; i++) {
@@ -118,6 +200,10 @@ public class Matrix implements Cloneable {
         return rtn;
     }
 
+    /** Scalar multiplication assignment operator.
+     * @param lambda Scalar to multiply each matrix component by.
+     * @return A clone of this instance after it has been multiplied.
+     */
     public Matrix scaleEquals(double lambda) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -127,6 +213,16 @@ public class Matrix implements Cloneable {
         return (Matrix)this.clone();
     }
 
+    /** Vector multiplication operator. Vector multiplication is right-handed
+     * so that the matrix columns are where the ijk unit vectors map to, instead
+     * of rows. This is to increases 'readability' of the matrix, and is also
+     * the rationale behind why the @link com.qbizzle.Math.Matrix#getColumn
+     * getColumn() @endlink is not a @em getRow() method.
+     * @param rhs Vector to multiply matrix by. This is the vector that is to be operated
+     *            on by the Matrix, that is to say the vector that will be rotated by this
+     *            matrix.
+     * @return The vector after being rotated by this matrix.
+     */
     public Vector mult(Vector rhs) {
         Vector rtn = new Vector();
         for (int i = 0; i < 3; i++) {
@@ -139,6 +235,10 @@ public class Matrix implements Cloneable {
         return rtn;
     }
 
+    /** Matrix multiplication operator. Used to compound multiple rotation matrices.
+     * @param rhs Matrix to compound this rotation by.
+     * @return A single matrix composed of the two rotations.
+     */
     public Matrix mult(Matrix rhs) {
         Matrix rtn = new Matrix();
         for (int i = 0; i < 3; i++) {
@@ -153,6 +253,10 @@ public class Matrix implements Cloneable {
         return rtn;
     }
 
+    /** Matrix multiplication assignment operator.
+     * @param rhs Matrix to compound this rotation by.
+     * @return A clone of this matrix after it has been multiplied.
+     */
     public Matrix multEquals(Matrix rhs) {
         Matrix tmp = this.mult(rhs);
         for (int i = 0; i < 3; i++) {
