d4cfd548ac35d6565fee5dc93b30ffbaf867d2c1
qbarnes <quinton.barnes68@gmail.com>
2022-01-27 23:01:34 -0600

    driver for plotting satellite ground track

--- src/com/qbizzle/Main.java
+++ src/com/qbizzle/Main.java
@@ -1,44 +1,65 @@
 package com.qbizzle;
 
 import com.qbizzle.Math.JD;
+import com.qbizzle.Math.OrbitalMath;
 import com.qbizzle.Math.Vector;
 import com.qbizzle.Orbit.BadTLEFormatException;
+import com.qbizzle.Orbit.StateVectors;
+import com.qbizzle.Orbit.TLE;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
 import static java.lang.Math.toRadians;
 
 public class Main {
 
     Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, BadTLEFormatException {
-//        double siderealTime = ST(new JD(new Date()), -6.0);
-////        int hour = (int) siderealTime;
-////        siderealTime -= hour;
-////        int minute = (int)(siderealTime * 60.0);
-////        siderealTime -= (minute / 60);
-////        double second = siderealTime * 60.0;
-////        System.out.println("Sidereal Time: " + hour + ":" + minute + ":" + second);
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, BadTLEFormatException, IOException {
+        String strZarya = "ISS (ZARYA)             \n" +
+                "1 25544U 98067A   22027.75547926  .00007563  00000+0  14194-3 0  9990\n" +
+                "2 25544  51.6451 312.0370 0006903  67.9238  24.2396 15.49669141323407";
+        TLE tleZarya = new TLE(strZarya);
+//        JD currentPosTime = new JD(1, 28, 2022, 18, 0, 0.0); // UTC
+//        StateVectors state = new StateVectors(tleZarya, currentPosTime.Difference(new JD(tleZarya)));
+//        System.out.println(state.Position());
+//        double siderealTime = ST(currentPosTime, 0.0);
+//        System.out.println("sidereal time: " + siderealTime);
 //        double offsetAngle = siderealTime / 24.0 * 360.0;
-//        String strZarya = "ISS (ZARYA)\n" +
-//                "1 25544U 98067A   08264.51782528  .00007752  00000+0  14579-3 0  9998\n" +
-//                "2 25544  51.6445 346.8728 0006795  43.1425  16.8937 15.49568201322317";
-//        // 22020.72035763 replace the epoch to this
-//        TLE tleZarya;
-//        try {
-//             tleZarya = new TLE(strZarya);
-//        } catch(BadTLEFormatException ex) {
-//            System.out.println("Bad tle format");
-//            throw new InternalError("Bad tle format");
-//        }
-//        StateVectors state = new StateVectors(tleZarya);
-//        System.out.println(state.Position().toString());
-//        System.out.println(offsetAngle);
+//        System.out.println("offset angle: " + offsetAngle);
+//
 //        Vector issPos = Rotate(state.Position(), -offsetAngle);
-//        System.out.println(issPos.toString());
+//        System.out.println("iss position: " + issPos.toString());
+//
+//        double [] latlng = toLatLng(issPos);
+//        System.out.println("lat: " + Math.toDegrees(latlng[0]));
+//        System.out.println("long: " + Math.toDegrees(latlng[1]));
+
+        double dt = 10 / 86400.0; // days
+        double timeToPlot = 1.0; //2.0 / tleZarya.MeanMotion(); // days
+        int numItr = (int)(timeToPlot / dt);
+
+        JD startTime = new JD(tleZarya);
+        String filename = "output/iss_1day_10sec.csv";
+        FileWriter writer = new FileWriter(filename);
+        writer.write("latitude, longitude\n");
+        for (int i = 0; i < numItr; i++) {
+            JD currentTime = startTime.Future(dt * i);
+            StateVectors state = new StateVectors(tleZarya, dt * i);
+            double offsetAngle = ST(currentTime, 0.0) / 24.0 * 360.0;
+            Vector currentPosition = Rotate(state.Position(), -offsetAngle);
+            double[] latlng = toLatLng(currentPosition);
+            writer.write(Math.toDegrees(latlng[0]) + ", " + Math.toDegrees(latlng[1]) + '\n');
+        }
+
+
     }
+
+    //14:54
 
     // time zone should be relative to GMT (CST = -6.0)
     public static double ST(JD now, double tZone) {
@@ -62,6 +83,15 @@ public class Main {
         double v3 = vec.x() * (Math.sin(aop)*Math.sin(inc))
                 + vec.y() * (Math.cos(aop)*Math.sin(inc));
         return new Vector(v1, v2, v3);
+    }
+
+    public static double[] toLatLng(Vector position) {
+        double xyMag = sqrt(Math.pow(position.x(), 2) + Math.pow(position.y(), 2));
+        double lat = Math.atan2(position.z(), xyMag);
+        double lng = OrbitalMath.atan2(position.y(), position.x());
+        if (lng > Math.PI) lng -= 2*Math.PI;
+
+        return new double[]{lat, lng};
     }
 
 }
