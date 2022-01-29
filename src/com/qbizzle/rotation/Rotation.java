a60c08e0121a2ab7091717586bd427d4058be653
qbarnes <quinton.barnes68@gmail.com>
2022-01-29 13:52:32 -0600

    added Rotate methods

--- src/com/qbizzle/rotation/Rotation.java
+++ src/com/qbizzle/rotation/Rotation.java
@@ -1,16 +1,19 @@
 package com.qbizzle.rotation;
 
-import com.qbizzle.exception.InvalidEulerRotationLengthException;
 import com.qbizzle.exception.InvalidAxisException;
+import com.qbizzle.exception.InvalidEulerRotationLengthException;
 import com.qbizzle.math.Matrix;
+import com.qbizzle.math.Vector;
 import com.qbizzle.referenceframe.Axis;
+import com.qbizzle.referenceframe.ReferenceFrame;
 
 import java.util.HashMap;
 import java.util.Map;
 
+//@todo create an euler angles class as a container for all three euler angles (to be more explicit than a double array).
 public class Rotation {
 
-    private static Map<String, Axis.Direction> axisMap = new HashMap<String, Axis.Direction>() {{
+    private final static Map<String, Axis.Direction> axisMap = new HashMap<>() {{
         put("x", Axis.Direction.X); put("y", Axis.Direction.Y); put("z", Axis.Direction.Z);
     }};
 
@@ -22,7 +25,6 @@ public class Rotation {
             default -> xRotationMatrix(Math.toRadians(angle));
         };
     }
-
 
     // intrinsic rotation
     public static Matrix getEulerMatrix(String axisOrder, double alpha, double beta, double gamma)
@@ -39,6 +41,26 @@ public class Rotation {
         return getMatrix(axisMap.get(axisOrder.substring(0, 1)), alpha)
                 .mult( getMatrix(axisMap.get(axisOrder.substring(1, 2)), beta)
                         .mult( getMatrix(axisMap.get(axisOrder.substring(2, 3)), gamma) ) );
+    }
+
+    public static Vector Rotate(Matrix rotMatrix, Vector vector) {
+        return rotMatrix.mult(vector);
+    }
+
+    public static Vector Rotate(Axis.Direction axis, double angle, Vector vector) {
+        return Rotate(getMatrix(axis, angle), vector);
+    }
+
+    public static Vector Rotate(String axisOrder, double alpha, double beta, double gamma, Vector vector)
+            throws InvalidEulerRotationLengthException, InvalidAxisException {
+        return Rotate(getEulerMatrix(axisOrder, alpha, beta, gamma), vector);
+    }
+
+    // struggling on this one hard. will come back to it later
+//    @todo not sure about the logic here, need to double check
+    public static Vector Rotate(ReferenceFrame from, ReferenceFrame to, Vector vector) {
+        Matrix rotation = to.toMatrix().mult(from.toMatrix().transpose());
+        return rotation.mult(vector);
     }
 
     // alpha in radians
