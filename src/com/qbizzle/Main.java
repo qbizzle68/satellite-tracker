3c6e95b4336ab2dc5eaeaba9d4adde3fb7610563
qbarnes <quinton.barnes68@gmail.com>
2022-01-30 22:50:56 -0600

    updating .class files

--- src/com/qbizzle/Main.java
+++ src/com/qbizzle/Main.java
@@ -1,17 +1,19 @@
 package com.qbizzle;
 
 import com.qbizzle.exception.InvalidTLEException;
-import com.qbizzle.math.JD;
 import com.qbizzle.math.OrbitalMath;
 import com.qbizzle.math.Vector;
 import com.qbizzle.orbit.StateVectors;
 import com.qbizzle.orbit.TLE;
+import com.qbizzle.time.JD;
+import com.qbizzle.time.SiderealTime;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
+import static com.qbizzle.math.OrbitalMath.EARTH_EQUITORIAL_RADIUS;
+import static com.qbizzle.math.OrbitalMath.EARTH_POLAR_RADIUS;
 import static java.lang.Math.sqrt;
 import static java.lang.Math.toRadians;
 
@@ -20,51 +22,44 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InvalidTLEException, IOException {
-        String strZarya = "ISS (ZARYA)             \n" +
-                "1 25544U 98067A   22027.75547926  .00007563  00000+0  14194-3 0  9990\n" +
-                "2 25544  51.6451 312.0370 0006903  67.9238  24.2396 15.49669141323407";
+        String strZarya = """
+                ISS (ZARYA)            \s
+                1 25544U 98067A   22030.51179398  .00005765  00000+0  11002-3 0  9999
+                2 25544  51.6444 298.3935 0006761  77.9892 281.4353 15.49702707323823""";
         TLE tleZarya = new TLE(strZarya);
-//        JD currentPosTime = new JD(1, 28, 2022, 18, 0, 0.0); // UTC
-//        StateVectors state = new StateVectors(tleZarya, currentPosTime.Difference(new JD(tleZarya)));
-//        System.out.println(state.Position());
-//        double siderealTime = ST(currentPosTime, 0.0);
-//        System.out.println("sidereal time: " + siderealTime);
-//        double offsetAngle = siderealTime / 24.0 * 360.0;
-//        System.out.println("offset angle: " + offsetAngle);
+        JD currentPosTime = new JD(1, 30, 2022, 23, 42, 0.0); // UTC
+        StateVectors state = new StateVectors(tleZarya, currentPosTime.Difference(new JD(tleZarya)));
+        System.out.println(state.Position());
+        double siderealTime = SiderealTime.siderealTime(currentPosTime, 0.0);
+        System.out.println("sidereal time: " + siderealTime);
+        double offsetAngle = siderealTime / 24.0 * 360.0;
+        System.out.println("offset angle: " + offsetAngle);
+
+        Vector issPos = Rotate(state.Position(), -offsetAngle);
+        System.out.println("iss position: " + issPos.toString());
+
+        double [] latlng = toLatLng(issPos);
+        System.out.println("declination: " + Math.toDegrees(latlng[0]));
+        System.out.println("lat: " + Math.toDegrees( geocentricToGeodetic(latlng[0]) ));
+        System.out.println("long: " + Math.toDegrees(latlng[1]));
+
+//        double dt = 10 / 86400.0; // days
+//        double timeToPlot = 1.0; //2.0 / tleZarya.MeanMotion(); // days
+//        int numItr = (int)(timeToPlot / dt);
 //
-//        Vector issPos = Rotate(state.Position(), -offsetAngle);
-//        System.out.println("iss position: " + issPos.toString());
-//
-//        double [] latlng = toLatLng(issPos);
-//        System.out.println("lat: " + Math.toDegrees(latlng[0]));
-//        System.out.println("long: " + Math.toDegrees(latlng[1]));
+//        JD startTime = new JD(tleZarya);
+//        String filename = "output/iss_1day_10sec.csv";
+//        FileWriter writer = new FileWriter(filename);
+//        writer.write("latitude, longitude\n");
+//        for (int i = 0; i < numItr; i++) {
+//            JD currentTime = startTime.Future(dt * i);
+//            StateVectors state = new StateVectors(tleZarya, dt * i);
+//            double offsetAngle = ST(currentTime, 0.0) / 24.0 * 360.0;
+//            Vector currentPosition = Rotate(state.Position(), -offsetAngle);
+//            double[] latlng = toLatLng(currentPosition);
+//            writer.write(Math.toDegrees(latlng[0]) + ", " + Math.toDegrees(latlng[1]) + '\n');
+//        }
 
-        double dt = 10 / 86400.0; // days
-        double timeToPlot = 1.0; //2.0 / tleZarya.MeanMotion(); // days
-        int numItr = (int)(timeToPlot / dt);
-
-        JD startTime = new JD(tleZarya);
-        String filename = "output/iss_1day_10sec.csv";
-        FileWriter writer = new FileWriter(filename);
-        writer.write("latitude, longitude\n");
-        for (int i = 0; i < numItr; i++) {
-            JD currentTime = startTime.Future(dt * i);
-            StateVectors state = new StateVectors(tleZarya, dt * i);
-            double offsetAngle = ST(currentTime, 0.0) / 24.0 * 360.0;
-            Vector currentPosition = Rotate(state.Position(), -offsetAngle);
-            double[] latlng = toLatLng(currentPosition);
-            writer.write(Math.toDegrees(latlng[0]) + ", " + Math.toDegrees(latlng[1]) + '\n');
-        }
-
-
-    }
-
-    //14:54
-
-    // time zone should be relative to GMT (CST = -6.0)
-    public static double ST(JD now, double tZone) {
-        double dt = now.Value() - (tZone / 24.0) - JD.J2000;
-        return (18.697_374_558 + 24.065_709_824_419_08 * dt) % 24.0;
     }
 
     // rotate vector around polar axis
@@ -92,6 +87,12 @@ public class Main {
         if (lng > Math.PI) lng -= 2*Math.PI;
 
         return new double[]{lat, lng};
+    }
+
+    public static double geocentricToGeodetic(double lat) {
+        final double flattening = (EARTH_EQUITORIAL_RADIUS - EARTH_POLAR_RADIUS) / EARTH_EQUITORIAL_RADIUS;
+        double lhs = Math.tan(lat) / Math.pow(1.0 - flattening, 2);
+        return Math.atan(lhs);
     }
 
 }
