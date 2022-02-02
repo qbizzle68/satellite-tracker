6b796b1f1a24d798e2482c602952a48b978ffae0
qbarnes <quinton.barnes68@gmail.com>
2022-02-02 17:36:30 -0600

    refactored and added local sidereal time

--- src/com/qbizzle/time/SiderealTime.java
+++ src/com/qbizzle/time/SiderealTime.java
@@ -13,12 +13,38 @@ public class SiderealTime {
      * Earth's geographic reference frame offset from the geocentric equitorial
      * celestial reference frame (offset of GMT to the first point of Aries).
      * @param julianDay The time to convert.
-     * @param timeZone The timezone of the time to convert.
      * @return The local sidereal time of Greenwich, England.
      */
-    public static double ST(JD julianDay, double timeZone) {
-        double dt = julianDay.Value() - (timeZone / 24.0) - JD.J2000;
+    public static double ST(JD julianDay) {
+        double dt = julianDay.Value() - JD.J2000;
         return (18.697_374_558 + 24.065_709_824_419_08 * dt) % 24.0;
+    }
+
+//    /** Converts a certain solar time into sidereal time, useful for computing
+//     * Earth's geographic reference frame offset from the geocentric equitorial
+//     * celestial reference frame (offset of GMT to the first point of Aries).
+//     * @param julianDay The time to convert.
+//     * @param timeZone The timezone of the time to convert.
+//     * @return The local sidereal time of Greenwich, England.
+//     */
+//    public static double ST(JD julianDay, double timeZone) {
+//        double dt = julianDay.Value() - (timeZone / 24.0) - JD.J2000;
+//        return (18.697_374_558 + 24.065_709_824_419_08 * dt) % 24.0;
+//    }
+
+    /** Converts a certain solar time into local sidereal time, useful for computing
+     * your longitudinal offset from the geocentric equitorial celestial reference frame
+     * (offset of your longitude to the first point of Aries).
+     * @param julianDay The time to convert.
+     * @param longitude The local longitude.
+     * @return The local sidereal time.
+     */
+    public static double LST(JD julianDay, double longitude) {
+        double lng2RA = ((longitude % 180.0) / 360.0) * 24.0;
+        double LST = ST(julianDay) + lng2RA;
+        if (LST < 0.0) return LST + 24.0;
+        else if (LST > 24.0) return LST - 24.0;
+        else return LST;
     }
 
 }
