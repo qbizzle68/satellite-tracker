352634a816a47cf5b6fd3bce025a6628551e3689
qbarnes <quinton.barnes68@gmail.com>
2022-02-18 12:44:25 -0600

    added overridden methods

--- src/com/qbizzle/tracking/AltAz.java
+++ src/com/qbizzle/tracking/AltAz.java
@@ -7,10 +7,12 @@ package com.qbizzle.tracking;
 
 import com.qbizzle.time.JD;
 
+import java.util.Objects;
+
 /** Container class for holding altitude and azimuth angles
  * for a position relative to a GeoPosition.
  */
-public class AltAz {
+public class AltAz implements Cloneable {
 
     private double altitude;
     private double azimuth;
@@ -31,6 +33,30 @@ public class AltAz {
         this.altitude = altitude;
         this.epoch = epoch;
     }
+
+    @Override
+    public String toString() {
+        return "AltAz{" +
+                "altitude=" + altitude +
+                ", azimuth=" + azimuth +
+                ", epoch=" + epoch +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AltAz altAz = (AltAz) o;
+        return Double.compare(altAz.altitude, altitude) == 0 && Double.compare(altAz.azimuth, azimuth) == 0 && epoch.equals(altAz.epoch);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(altitude, azimuth, epoch);
+    }
+
+    // add clone once JD is cloneable
 
     /** Retrieves the azimuth of this object.
      * @return The azimuth angle.
